abe1d92a786d4dd098e84d647433fb1148e7433b
Takeshi Miyajima <rozyhead@gmail.com>
2018-07-10 11:49:23 +0900

    Update discovery-service

--- discovery-service/src/main/kotlin/com/github/rozyhead/springclouddemo/discovery/DiscoveryServiceApplication.kt
+++ discovery-service/src/main/kotlin/com/github/rozyhead/springclouddemo/discovery/DiscoveryServiceApplication.kt
@@ -1,4 +1,4 @@
-package com.github.rozyhead.springclouddemo
+package com.github.rozyhead.springclouddemo.discovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
