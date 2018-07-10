0c8c2c036855acc025dd73abea9b6d6041566ee1
Takeshi Miyajima <rozyhead@gmail.com>
2018-07-10 11:21:22 +0900

    Add discovery-service

--- discovery-service/src/main/kotlin/com/github/rozyhead/springclouddemo/discovery/DiscoveryServiceApplication.kt
+++ discovery-service/src/main/kotlin/com/github/rozyhead/springclouddemo/discovery/DiscoveryServiceApplication.kt
@@ -2,10 +2,12 @@ package com.github.rozyhead.springclouddemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SpringCloudDemoApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringCloudDemoApplication>(*args)
+  runApplication<SpringCloudDemoApplication>(*args)
 }
